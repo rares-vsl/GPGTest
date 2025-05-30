2f1b6061ffe81a5d18c885b256d4a88d69a9ff96
rares vasiliu <rv.vasiliu@gmail.com>
2025-05-30 11:12:47 +0200

    feat: double msg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // print hello world
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
