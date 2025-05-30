96f4c7b7c4a6f5d20cc1eb20de83ba91f6a95821
rares vasiliu <rv.vasiliu@gmail.com>
2025-05-30 11:15:41 +0200

    fix: correct msg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // print hello world
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Its me");
     }
 }
